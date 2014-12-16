c2f3bf674d5c0f9f42262feba9541ffa4da69da6
Ahmed Abdelhady <ahmed.abdelhady@git.ttu.ee>
2014-12-16 14:02:22 +0200

    added changes to GUI

--- gen/com/example/shareaview/R.java
+++ gen/com/example/shareaview/R.java
@@ -409,9 +409,9 @@ containing a value of this type.
         public static final int powered_by_google_light=0x7f020020;
     }
     public static final class id {
-        public static final int ToCapture=0x7f070019;
-        public static final int ToMap=0x7f07001a;
-        public static final int action_settings=0x7f070022;
+        public static final int ToCapture=0x7f070018;
+        public static final int ToMap=0x7f070019;
+        public static final int action_settings=0x7f070021;
         public static final int book_now=0x7f070010;
         public static final int buyButton=0x7f07000a;
         public static final int buy_now=0x7f07000f;
@@ -425,7 +425,7 @@ containing a value of this type.
         public static final int holo_dark=0x7f070005;
         public static final int holo_light=0x7f070006;
         public static final int hybrid=0x7f070004;
-        public static final int map=0x7f07001b;
+        public static final int map=0x7f07001a;
         public static final int match_parent=0x7f07000c;
         public static final int monochrome=0x7f070013;
         public static final int none=0x7f070000;
@@ -436,13 +436,12 @@ containing a value of this type.
         public static final int selectionDetails=0x7f07000b;
         public static final int strict_sandbox=0x7f070009;
         public static final int terrain=0x7f070003;
-        public static final int textView1=0x7f070018;
-        public static final int user_direction=0x7f070020;
-        public static final int view_description=0x7f07001c;
-        public static final int view_direction=0x7f07001f;
-        public static final int view_image=0x7f070021;
-        public static final int view_lat=0x7f07001d;
-        public static final int view_long=0x7f07001e;
+        public static final int user_direction=0x7f07001f;
+        public static final int view_description=0x7f07001b;
+        public static final int view_direction=0x7f07001e;
+        public static final int view_image=0x7f070020;
+        public static final int view_lat=0x7f07001c;
+        public static final int view_long=0x7f07001d;
         public static final int wrap_content=0x7f07000d;
     }
     public static final class integer {
